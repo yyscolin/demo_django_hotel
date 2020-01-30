6e87366acf26b8dce3cc453a9b51e22b7a15b484
yyscolin <48078302+yyscolin@users.noreply.github.com>
2020-01-30 16:14:43 +0800

    Renamed project name for AWS integration

--- prjhdd/wsgi.py
+++ prjhdd/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hotel_demo project.
+WSGI config for prjhdd project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hotel_demo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prjhdd.settings")
 
 application = get_wsgi_application()
